b3e90caa570f6bd424790b8c5ab9035e9628c6ec
John Moon <johnmoonyy@gmail.com>
2013-12-19 14:11:58 -0500

    Validates presence of Categorization

--- spec/models/categorization_spec.rb
+++ spec/models/categorization_spec.rb
@@ -3,4 +3,7 @@ require 'spec_helper'
 describe Categorization do
   it { should belong_to(:book) }
   it { should belong_to(:category) }
+  it { should validate_presence_of(:book) }
+  it { should validate_presence_of(:category) }
+
 end
